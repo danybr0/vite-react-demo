b22d3143306a89871b9de95601e44d3bd78d26e5
danybr0 <dan.covic@outlook.com>
2023-10-21 15:40:57 +0200

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -28,9 +31,6 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p> */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
